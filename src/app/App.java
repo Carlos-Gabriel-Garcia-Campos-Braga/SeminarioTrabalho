bc652ffd77f94056e62dbc30fc98b3c2c1039c3c
Leonardo Amichi <leoamichi1801@gmail.com>
2025-06-11 13:52:14 -0300

    fix: Melhorando a classe Carro

--- src/app/App.java
+++ src/app/App.java
@@ -5,14 +5,15 @@ import models.Carro;
 public class App {
     public static void main(String[] args) throws Exception {
         // Cria um objeto carro original
-        Carro carroOriginal = new Carro("Civic", "Preto", "AXB-123");
+        Carro carroOriginal = new Carro("Etios", "Toyota", 2020, "1.0 Turbo", "Aro 14", "Alcool", "ABS", 68.000);
 
         // Cria uma cópia usando o prototype(clone)
         Carro carroClone = carroOriginal.clone();
 
         // Modifica o clone para demonstrar que são objetos independentes na memória
-        carroClone.setCor("Vermelho");
-        carroClone.setPlaca("AXC-321");
+        carroClone.setMotor("1.0");
+        carroClone.setAno(2018);
+        carroClone.setPreco(59.000);
 
         //Saída no terminal para verificar a diferença
         System.out.println("Carro Original: " + carroOriginal);
