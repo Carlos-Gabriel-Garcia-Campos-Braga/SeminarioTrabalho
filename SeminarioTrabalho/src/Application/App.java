887c5b5b91797a6364f5c514ef1de03c623b6c6d
Joao Frederico <joaofrederico.xavier@gmail.com>
2025-06-13 01:08:17 -0300

    Casting de brand para carro

--- SeminarioTrabalho/src/Application/App.java
+++ SeminarioTrabalho/src/Application/App.java
@@ -14,14 +14,22 @@ public class App {
             c2.setAno(2018);
             c2.setPreco(59.000);
             
-            Brand corollas = new Brand(2020, "1.6", "Aro 16", "Diesel", "ABS", 70.000);
+            Brand corollas = new Brand();
+            
             corollas.setModelo("Corolla");
             corollas.setMarca("Toyota");
             Car c4 = corollas.clone();
-            
+            c4.setAno(2020);
+            c4.setCombustivel("Diesel");
+            c4.setFreio("ABS");
+            c4.setRodas("Aro 16");
+            c4.setMotor("1.6");
+            c4.setPreco(70.000);
+            Brand c5 = corollas.clone();
             System.out.println(c1);
             System.out.println(c2);
             System.out.println(c4);
+            System.out.println(c5);
         }
         catch(Exception ex)
         {
